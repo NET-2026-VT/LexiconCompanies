4bb2d91797470a1ce95c9bb7409956f92303961f
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-08-18 14:41:30 +0200

    Add Employee

--- Companies.API/Models/Entities/Company.cs
+++ Companies.API/Models/Entities/Company.cs
@@ -8,4 +8,7 @@ public class Company
     public required string Address { get; set; }
     public string? Country { get; set; }
 
+    //Navigation property
+    public IEnumerable<Employee> Employees { get; set; } = [];
+
 }
---
+++ Companies.API/Models/Entities/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace Companies.API.Models.Entities;
+
+public class Employee
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public  int Age { get; set; }
+    public required string Position { get; set; }
+
+    //FK
+    public Guid CompanyId { get; set; }
+
+    //Navigation property
+    public Company? Company { get; set; }
+}
