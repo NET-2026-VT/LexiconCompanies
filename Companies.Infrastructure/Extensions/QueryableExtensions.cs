de92ef8a7abf3dfbf2ab1dc5b7b3c81c4fefc910
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-09-01 15:30:31 +0200

    Change to interface return type on QuarebleExtension

--- Companies.Infrastructure/Extensions/QueryableExtensions.cs
+++ Companies.Infrastructure/Extensions/QueryableExtensions.cs
@@ -1,12 +1,13 @@
 ﻿using Companies.Infrastructure.Paging;
 using Companies.Shared.Paging;
+using Domain.Contracts;
 using Microsoft.EntityFrameworkCore;
 
 namespace Companies.Infrastructure.Extensions;
 
 public static class QueryableExtensions
 {
-    public static async Task<PagedList<T>> ToPagedListAsync<T>(
+    public static async Task<IPagedList<T>> ToPagedListAsync<T>(
         this IQueryable<T> query,
         QueryParameters parameters) where T : class
     {
