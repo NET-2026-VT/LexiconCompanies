4123f7ef2b045073a72cefc28aeee7b6dc2e68b3
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-09-03 13:57:53 +0200

    Start on Seed

--- Companies.API/Services/DataSeedService.cs
+++ Companies.API/Services/DataSeedService.cs
@@ -1,4 +1,5 @@
 ﻿using Bogus;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Companies.API.Services;
@@ -6,12 +7,19 @@ namespace Companies.API.Services;
 internal class DataSeedService : IHostedService
 {
     private readonly IServiceProvider serviceProvider;
+    private readonly IConfiguration configuration;
     private readonly ILogger<DataSeedService> logger;
     private List<Position> _positions = [];
+    private UserManager<Employee> userManager = null!;
+    private RoleManager<IdentityRole> roleManager = null!;
+    private const string EmployeeRole = "Employee";
+    private const string AdminRole = "Admin";
+    private const string TeamLead = "TeamLead";
 
-    public DataSeedService(IServiceProvider serviceProvider, ILogger<DataSeedService> logger)
+    public DataSeedService(IServiceProvider serviceProvider, IConfiguration configuration, ILogger<DataSeedService> logger)
     {
         this.serviceProvider = serviceProvider;
+        this.configuration = configuration;
         this.logger = logger;
     }
 
@@ -27,18 +35,28 @@ internal class DataSeedService : IHostedService
 
         if (await context.Companies.AnyAsync(cancellationToken)) return;
 
+        userManager = scope.ServiceProvider.GetRequiredService<UserManager<Employee>>()
+                            ?? throw new ArgumentNullException();
+
+        roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>()
+                            ?? throw new ArgumentNullException();
+
         try
         {
+
+            await CreateRolesAsync([AdminRole, EmployeeRole]);
+
             _positions =
                [
-                 new Position {Name = "Developer"},
+                  new Position {Name = "Developer"},
                   new Position {Name = "Tester"},
-                  new Position {Name = "Admin"},
+                  new Position {Name = TeamLead},
                 ];
 
             context.AddRange(_positions);
             IEnumerable<Company> companies = GenerateCompanies(10);
             context.Companies.AddRange(companies);
+            await GenerateEmployeesAsync(50, companies);
             await context.SaveChangesAsync(cancellationToken);
             logger.LogInformation("Seed complete");
         }
@@ -46,6 +64,19 @@ internal class DataSeedService : IHostedService
         {
             logger.LogError($"Data seed fail with message: {ex.Message}. Exceeption: {ex.InnerException}");
             throw;
+        }
+    }
+
+    private async Task CreateRolesAsync(string[] rolenames)
+    {
+        foreach (string rolename in rolenames)
+        {
+            if (await roleManager.RoleExistsAsync(rolename)) continue;
+            var role = new IdentityRole { Name = rolename };
+            var res = await roleManager.CreateAsync(role);
+
+            if (!res.Succeeded) throw new Exception
+                    (string.Join("\n", res.Errors.Select(e => $"{e.Code}: {e.Description}")));
         }
     }
 
@@ -60,22 +91,45 @@ internal class DataSeedService : IHostedService
                 StreetAddress = f.Address.StreetAddress(),
                 Country = f.Address.Country()
             };
-            c.Employees = GenerateEmployees(f.Random.Int(min: 2, max: 10));
+          //  c.Employees = GenerateEmployees(f.Random.Int(min: 2, max: 10));
         });
 
         return faker.Generate(numberOfCompanies);
     }
 
-    private ICollection<Employee> GenerateEmployees(int numberofEmployees)
+    private async Task GenerateEmployeesAsync(int numberofEmployees, IEnumerable<Company> companies)
     {
         var faker = new Faker<Employee>("sv").Rules((f, e) =>
         {
             e.Name = f.Person.FullName;
             e.Age = f.Random.Int(18, 70);
             e.Position = _positions[f.Random.Int(0, _positions.Count - 1)];
+            e.Email = f.Person.Email;
+            e.UserName = f.Person.UserName;
+            e.Company = f.PickRandom(companies);
         });
 
-        return faker.Generate(numberofEmployees);
+        var users = faker.Generate(numberofEmployees);
+
+        var teamlead = _positions.First(p => p.Name == TeamLead);
+
+        var password = configuration["password"];
+        ArgumentNullException.ThrowIfNull(password, nameof(password));
+
+        foreach (var user in users)
+        {
+            var result = await userManager.CreateAsync(user, password);
+
+            if (!result.Succeeded) 
+                throw new Exception(string.Join("\n", 
+                result.Errors.Select(e => $"{e.Code}: {e.Description}")));
+
+            var role = user.Position.Name == TeamLead
+                ? AdminRole
+                : EmployeeRole;
+
+            await userManager.AddToRoleAsync(user, role);
+        }
     }
 
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
